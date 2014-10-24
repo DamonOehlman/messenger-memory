f6ebb9c5bb31b8b885c47515156635496898e1eb
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-24 22:07:34 +1100

    Refactored to use pull-streams

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+var pull = require('pull-core');
+var pushable = require('pull-pushable');
 
+/**
+  # messenger-memory
+
+  An in-memory messaging helper.
+
+  Follows the [messenger-archetype](https://github.com/DamonOehlman/messenger-archetype)
+
+**/
+module.exports = function(opts) {
+  var buffers = [];
+  var broadcast = pull.Sink(function(read, source) {
+    function next(end, data) {
+      var targets;
+
+      if (end) {
+        return;
+      }
+
+      // write the data to each of the buffers that isn't us
+      buffers.filter(function(buffer) {
+        return buffer !== source;
+      }).forEach(function(buffer) {
+        buffer.push(data);
+      });
+
+      process.nextTick(function() {
+        read(null, next);
+      });
+    }
+
+    read(null, next);
+  });
+
+  return function(callback) {
+    var source = pushable();
+
+    buffers.push(source);
+    callback(null, source, broadcast(source));
+  };
+};
